3f18938440ac1f7e1a8d78f0502ce4c2f4a1677d
mohammadkhalaf <mskhala@emory.edu>
2020-10-01 23:56:54 -0500

    Completed Part 2 Required Stories

--- app/src/main/java/com/example/Aflam/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/Aflam/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.example.Aflam.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.Target;
+import com.example.Aflam.DetailActivity;
 import com.example.Aflam.R;
 import com.example.Aflam.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -60,6 +66,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
         // define member views for each member of viewholder
 
+        RelativeLayout container;
+
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -69,13 +77,30 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             // Use getter method on movie to populate each views
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).override(Target.SIZE_ORIGINAL).into(ivPoster);
+
+            // 1. Register click Listener on the whole container. We need to find the container layout reference.
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. Navigate to a new activity on tap using Intents
+                    Intent intent = new Intent(context, DetailActivity.class);
+                    // Pass data into the next activity
+                    intent.putExtra("title", movie.getTitle());
+                    // putExtra can take in a Parcelable value, so we can make movie parcelable to be able to just pass it in we're going to use a library (Parceler)
+                    intent.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(intent);
+                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/Aflam/models/Movie.java
+++ app/src/main/java/com/example/Aflam/models/Movie.java
@@ -3,21 +3,31 @@ package com.example.Aflam.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     // Encapsulates the idea of a movie
+    int movieId;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+    // required by Parceler library
+    public Movie() {
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -31,6 +41,11 @@ public class Movie {
 
     // lets make our poster path usable/ a full url, right now we just get a relative path
     // We'll use something called the configurations API response.
+
+    public int getMovieId() {
+        return movieId;
+    }
+
     public String getPosterPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
@@ -41,5 +56,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
     }
 }
